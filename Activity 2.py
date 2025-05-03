cbfbe6a1c9907609fd55761b3bf1048118c832af
mohdahmad2013 <ziena.saif@gmail.com>
2025-05-03 03:57:18 -0700

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,10 @@
+x={1,3,5,7,9,4,8}
+y={1,2,4,3,6,8,7,9}
+r=x.intersection(y) #intersection
+print(r)
+r1=x.union(y)#Union
+print(r1)
+r2=x.difference(y)#difference
+print(r2)
+r3=x.symmetric_difference(y)#symetric difference
+print(r3)
